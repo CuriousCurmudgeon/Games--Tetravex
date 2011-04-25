0834d0238557ea04696bec2c08cf2e8b1e4e85e7
Brian Meeker <meeker.brian@gmail.com>
2011-04-24 21:01:32 -0400

    Updated Games::Tetravex::Move to not allow an invalid swap. The test now passes. Still need to actually integrate this into the main Games::Tetravex module though.

--- lib/Games/Tetravex/Move.pm
+++ lib/Games/Tetravex/Move.pm
@@ -47,40 +47,52 @@ class Games::Tetravex::Move {
     # Is this move valid? This means that the colors/numbers
     # of all neighboring pieces match. If we are swapping pieces,
     # the swapped piece must be valid too.
+    # If we're swapping, both grids must be checked.
+    # Otherwise, only the to_grid needs checked.
     method is_valid() {
+	if ( defined $self->swapped_piece ) {
+	    return $self->_is_valid_helper($self->to_grid,   $self->to_index) && 
+		   $self->_is_valid_helper($self->from_grid, $self->from_index);
+	}
+	else {
+	    return $self->_is_valid_helper($self->to_grid, $self->to_index);
+	}
+    }
+
+    method _is_valid_helper($grid, $index) {
 	# If we aren't moving the piece to a grid that requires
 	# valid moves, then just return 1.
-	if ( !$self->to_grid->requires_valid ) {
+	if ( !$grid->requires_valid ) {
 	    return 1;
 	}
+
 	# Look at each neighboring position. If that position
 	# is outside of the grid, then we don't care about it.
-	my $to_index = $self->to_index;
 
 	# Look above.
-	if ( ($to_index - 3) >= 0 ) {
-	    my $above = $self->to_grid->pieces->[$to_index - 3];
+	if ( ($index - 3) >= 0 ) {
+	    my $above = $grid->pieces->[$index - 3];
 	    if (defined $above && $self->piece->top != $above->bottom ) {
 		return 0;
 	    }
 	}
 	# Look below
-	if ( ($to_index + 3) <= 8 ) {
-	    my $below = $self->to_grid->pieces->[$to_index + 3];
+	if ( ($index + 3) <= 8 ) {
+	    my $below = $grid->pieces->[$index + 3];
 	    if (defined $below && $self->piece->bottom != $below->top ) {
 		return 0;
 	    }
 	}
 	# Look to the right
-	if ( ($to_index % 3) != 2 ) {
-	    my $right = $self->to_grid->pieces->[$to_index + 1];
+	if ( ($index % 3) != 2 ) {
+	    my $right = $grid->pieces->[$index + 1];
 	    if (defined $right && $self->piece->right != $right->left ) {
 		return 0;
 	    }
 	}
 	# Look to the left
-	if ( ($to_index % 3) != 0 ) {
-	    my $left = $self->to_grid->pieces->[$to_index - 1];
+	if ( ($index % 3) != 0 ) {
+	    my $left = $grid->pieces->[$index - 1];
 	    if (defined $left && $self->piece->left != $left->right ) {
 		return 0;
 	    }
