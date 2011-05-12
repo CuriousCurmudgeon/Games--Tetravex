8e94bf42d54de1bc44ef6172a6ee75ce0763e11c
CuriousCurmudgeon <meeker.brian@gmail.com>
2011-05-12 11:36:47 -0400

    Fixed tesT

--- t/Games/Tetravex/Piece.pl
+++ t/Games/Tetravex/Piece.pl
@@ -36,7 +36,7 @@ sub startup : Tests(startup) {
 
 sub can_create_instance : Tests {
     can_ok('Games::Tetravex::Piece', 'new');
-    my $piece = Games::Tetravex::Piece->new(value => [0, 1, 2, 3], x => 100, y => 100, $font);
+    my $piece = Games::Tetravex::Piece->new(value => [0, 1, 2, 3], x => 100, y => 100, font => $font);
     isa_ok ($piece, 'Games::Tetravex::Piece', 'the newly created object is a Piece');
 }
 
